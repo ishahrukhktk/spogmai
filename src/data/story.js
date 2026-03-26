fc440d2f8fdf41f0982486be9856e8f0f996af88
ishahrukhktk <109621923+ishahrukhktk@users.noreply.github.com>
2026-03-26 19:43:24 +0500

    Update story.js

--- src/data/story.js
+++ src/data/story.js
@@ -1,28 +1,28 @@
-import img1 from "../assets/images/img1.jpg";
-import img2 from "../assets/images/img2.jpg";
-import img3 from "../assets/images/img3.jpg";
-import img4 from "../assets/images/img4.jpg";
-import img5 from "../assets/images/img5.jpg";
+import img1 from "../assets/images/img1.jpeg";
+import img2 from "../assets/images/img2.jpeg";
+import img3 from "../assets/images/img3.jpeg";
+import img4 from "../assets/images/img4.jpeg";
+import img5 from "../assets/images/img5.jpeg";
 
 export const story = [
   {
     image: img1,
-    text: "Hiiii Goluuuu, Just wanted to tell you somethingggg...",
+    text: "hiii meri moto, meri spogmai, just wanted to tell you something....",
   },
   {
     image: img2,
-    text: "Do you remember this picture? Our first picture that you took ✨",
+    text: "Do you remember this picture? Our first picture together hehe you look bht piyari in this ✨",
   },
   {
     image: img3,
-    text: "Our first trip as a hackathon... 🥺",
+    text: "everytime i see you something insdie me get hard ugh. 🥺",
   },
   {
     image: img4,
-    text: "A day full of enjoyymenttt 😌",
+    text: "and you know what you are mine i love you acha mela bacha meli jelebi😌",
   },
   {
     image: img5,
-    text: "Yeah, sometimes I do things that make you angry, and I am sorry for that. But just like this image, I choose US. So maaf kar de goluu ❤️",
+    text: "Yeah, sometimes I do things that make you angry, and I am sorry for that. but every day i wake up i choose you over everything. So maaf kar de motoooo ❤️",
   },
 ];
